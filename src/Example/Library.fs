496b304c920f59dc776aa06ce52fdf32a5a5b5f9
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2020-02-07 14:22:18 -0300

    moved examples to use paket too

--- src/Example/Library.fs
+++ src/Example/Library.fs
@@ -2,4 +2,6 @@
 
 [<Myriad.Core.MyriadGenerator("fields")>]
 type Test1 = { one: int; two: string; three: float; four: float32 }
+
+[<Myriad.Core.MyriadGenerator("fields")>]
 type Test2 = { one: Test1; two: string }
